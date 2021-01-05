40ceadbf1d338011c1d84f41e3524b2472bd00f0
Leslie <lesliemurias94@gmail.com>
2021-01-05 14:59:42 -0600

    creando migraciones a la bd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,16 +11,17 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up()//este metodo se ejecuta automatico cuando por consola hacemos migrate
     {
+        //crear la tablas en la base de datos con las siguientes columnas
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id();//integre auto-incremenable
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();//campo de correo que no se repite en la bd
+            $table->timestamp('email_verified_at')->nullable();//nullable es la propiedad que indica que esta columna puede quedar vacia
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//este metodo crea dos columnas create_up update_up
         });
     }
 
